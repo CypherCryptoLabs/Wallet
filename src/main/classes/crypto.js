d4af5ddd9bcdee0e5475b43ec4820abe700a70b0
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2022-07-05 15:20:24 +0200

    signature and remove log

--- src/main/classes/crypto.js
+++ src/main/classes/crypto.js
@@ -1,6 +1,7 @@
 const ellipticcurve = require("@starkbank/ecdsa-node");
 const PrivateKey = ellipticcurve.PrivateKey;
 const PublicKey = ellipticcurve.PublicKey;
+const Ecdsa = ellipticcurve.Ecdsa;
 
 const crypto = require("crypto")
 
@@ -9,8 +10,6 @@ class Crypto {
         this._privateKey = (key == undefined) ? this.generateKey() : PrivateKey.fromPem(key);
         this._publicKey = this._privateKey.publicKey();
         this.blockchainAddress = crypto.createHash('sha256').update(Buffer.from(this._publicKey.toPem().toString().replace("-----BEGIN PUBLIC KEY-----\n", "").replace("\n", "").replace("-----END PUBLIC KEY-----", ""), "base64")).digest('hex');
-        
-        console.log(this.privateKey + "\n" + this.publicKey + "\n" + this.blockchainAddress)
     }
 
     get publicKey() {
@@ -23,6 +22,10 @@ class Crypto {
 
     generateKey() {
         return new PrivateKey();;
+    }
+
+    sign(packet) {
+        return Ecdsa.sign(packet, this._privateKey).toBase64();
     }
 }
 
