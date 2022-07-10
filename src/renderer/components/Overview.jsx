ba8957a239e677ede7159c9706c37881898be88e
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2022-07-10 17:11:32 +0200

    display error message if Wallet could not be synced

--- src/renderer/components/Overview.jsx
+++ src/renderer/components/Overview.jsx
@@ -5,7 +5,10 @@ import { Link } from 'react-router-dom';
 export class Overview extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {balance: 0}
+        this.state = {
+          balance: 0,
+          error: false
+        }
         this.syncToNetwork = this.syncToNetwork.bind(this)
     }
 
@@ -15,8 +18,13 @@ export class Overview extends React.Component {
     }
 
     async syncToNetwork() {
+      try {
         await window.electron.ipcRenderer.syncToNetwork();
         this.setState({balance: await window.electron.ipcRenderer.getBalance()});
+      } catch (error) {
+        console.log(error);
+        this.setState({balance: await window.electron.ipcRenderer.getBalance(), error: true});
+      }
     }
 
     render() {
@@ -24,13 +32,17 @@ export class Overview extends React.Component {
 
         return (
           <div className='bg-black min-h-screen min-w-screen text-white font-black'>
-              <h1 className='text-4xl text-gradient m-auto pt-10'>Balance:</h1>
-              <h2 className='m-auto text-2xl'>{this.state.balance} Cypher</h2>
-              <div className='m-auto flex flex-wrapt space-x-10 mt-10'>
-                <Link to="/receive" className='text-xl bg-secondary px-3 py-2 rounded-xl w-40 text-center hover:scale-110 duration-300'>Receive</Link>
-                <Link to="/send" className='text-xl bg-secondary px-3 py-2 rounded-xl w-40 text-center hover:scale-110 duration-300'>Send</Link><br />
-              </div>
+            <h1 className='text-4xl text-gradient m-auto pt-10'>Balance:</h1>
+            <h2 className='m-auto text-2xl'>{this.state.balance} Cypher</h2>
+            <div className='m-auto flex flex-wrapt space-x-10 mt-10'>
+              <Link to="/receive" className='text-xl bg-secondary px-3 py-2 rounded-xl w-40 text-center hover:scale-110 duration-300'>Receive</Link>
+              <Link to="/send" className='text-xl bg-secondary px-3 py-2 rounded-xl w-40 text-center hover:scale-110 duration-300'>Send</Link><br />
             </div>
+            {
+              this.state.error == true &&
+              <p className='text-center mt-4 border-2 border-red-800 rounded-xl w-9/12 m-auto bg-red-800/20'>Your wallet could not be synced!</p>
+            }
+          </div>
           );
     }
 }
