223934b7d798929c788c0ab4cce9a7122baaeeb6
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2022-07-11 13:16:25 +0200

    Receive style

--- src/renderer/components/Receive.jsx
+++ src/renderer/components/Receive.jsx
@@ -14,10 +14,11 @@ export class Receive extends React.Component {
 
     render() {
         return (
-          <div>
-            <h1>Receive Cypher</h1>
-            <Link to="../">Back</Link>
-            <input value={this.state.address} readOnly></input>
+          <div className='bg-black min-h-screen min-w-screen text-white font-black'>
+            <Link to="../" className='text-xl bg-secondary p-0 pl-[2px] leading-9 rounded-full w-9 h-9 text-center hover:scale-110 duration-300 mt-4 right-3 absolute'>X</Link>
+            <h1 className='text-4xl text-gradient m-auto pt-10'>Receive Cypher</h1>
+            <p className='m-auto mt-4'>Your Address is the following:</p>
+            <input className='block m-auto bg-secondary rounded-xl h-10 text-center mt-2 w-4/5' value={this.state.address} readOnly></input>
           </div>
         );
     }
