bc99bf2b2f8125f2e3eb27cbf286a5d40ae4bc4a
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2022-07-24 20:19:17 +0200

    coeherence

--- src/renderer/components/Settings.jsx
+++ src/renderer/components/Settings.jsx
@@ -8,7 +8,7 @@ export class Settings extends React.Component {
         return(
             <div className='bg-black min-h-screen min-w-screen text-white font-black'>
                 <Link to="../" className='text-xl bg-secondary p-0 pl-[2px] leading-9 rounded-full w-9 h-9 text-center hover:scale-110 duration-300 mt-4 right-3 absolute'>X</Link>
-                <h1 className='text-4xl text-gradient ml-4 pt-10'>Node Address & Port:</h1>
+                <h1 className='text-4xl text-gradient pt-10 m-auto'>Node Address & Port:</h1>
                 <div className='m-auto'>
                     <input className='m-auto bg-secondary rounded-xl h-10 text-center mt-4 w-44' placeholder="Address" name='receiver' onChange={this.onChange}></input><p className='inline'>:</p>
                     <input className='m-auto bg-secondary rounded-xl h-10 text-center mt-4 w-16' placeholder="Port" type="number" step="0.001" name='amount' onChange={this.onChange} min="0" max="65535"></input><br />
