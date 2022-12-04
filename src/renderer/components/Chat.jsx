b1f99bcffa98d30fe1f4bfbe42fa4bf16eaaedba
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2022-12-04 14:23:25 +0100

    simple UI for messenger

--- src/renderer/components/Chat.jsx
+++ src/renderer/components/Chat.jsx
@@ -7,7 +7,7 @@ export default function Chat(props) {
     // this is just a wrapper for the ChatComponent class
     const location = useLocation();
     console.log(location.state.key);
-    return <ChatComponent address={location.state.key} ></ChatComponent>
+    return <div className='bg-black min-h-screen min-w-screen text-white font-black'><ChatComponent address={location.state.key} ></ChatComponent></div>
 }
 
 class ChatComponent extends React.Component {
@@ -21,12 +21,15 @@ class ChatComponent extends React.Component {
 
     render() {
         return (
-            <>
-                <p>{this.address}</p>
-                <div className='fixed bottom-4 inset-x-16 m-auto flex flex-wrapt space-x-10 mt-10 text-white'>
-                <Link to="/" className='text-xl bg-secondary px-3 py-2 rounded-xl w-40 text-center hover:scale-110 duration-300'>Wallet</Link>
-                <Link to="/messenger" className='text-xl bg-secondary px-3 py-2 rounded-xl w-40 text-center hover:scale-110 duration-300'>Messenger</Link>
+            <>  
+                <div className='h-10'>
+                    <Link to="/messenger" className='text-xl bg-secondary p-0 px-3 leading-9 rounded-full h-9 text-center hover:scale-110 duration-300 mt-2 left-3 absolute'>&lt;</Link>
                 </div>
+                <div className='min-w-screen'>
+
+                </div>
+                <textarea className='absolute bottom-[1%] left-[1%] w-[78%] bg-secondary rounded-xl h-10' type="textarea"/>
+                <button className='absolute bottom-[1%] right-[1%] w-[18%] bg-secondary rounded-xl h-10'>Send</button>
             </>
         );
     }
