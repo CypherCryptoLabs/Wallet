dd624453a1b556b0f4a14f8717b1b140cb85dd4b
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2022-12-05 22:28:44 +0100

    always scroll to bottom

--- src/renderer/components/Chat.jsx
+++ src/renderer/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { render } from '@testing-library/react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom'
@@ -18,7 +18,9 @@ class ChatComponent extends React.Component {
         this.loadMessages = this.loadMessages.bind(this)
         this.onChange = this.onChange.bind(this)
         this.sendMessages = this.sendMessages.bind(this)
+        this.scrollToBottom = this.scrollToBottom.bind(this)
         this.timer = setInterval(this.loadMessages(), 100)
+        this.messagesEndRef = React.createRef()
         this.state = {
             messages: [],
             address: "",
@@ -29,6 +31,7 @@ class ChatComponent extends React.Component {
     async componentDidMount() {
         this.timer = setInterval(() => {this.loadMessages()}, 100)
         this.setState({address: await window.electron.ipcRenderer.getBlockchainAddress()});
+        this.scrollToBottom()
     }
 
     componentWillUnmount() {
@@ -50,22 +53,25 @@ class ChatComponent extends React.Component {
         console.log(this.state.messageToSend)
     }
 
+    async scrollToBottom() {
+        this.messagesEndRef.current?.scrollIntoView()
+    }
+
     render() {
         let messages = []
-        console.log(this.state.messages)
         if(this.state.messages != undefined) {
             for(var i = 0; i < this.state.messages.length; i++) {
                 let message = this.state.messages[i];
                 if(message.sender == this.state.address) {
                     messages.push(
-                        <div className='bg-secondary rounded-xl p-2 ml-auto' key={i}>
-                            <p>{message.message}</p>
+                        <div className='bg-secondary rounded-xl p-2 ml-auto mb-2 max-w-[80%] break-words' key={i}>
+                            <p className='whitespace-pre-wrap break-words max-w-[100%]'>{message.message}</p>
                         </div>
                     )
                 } else {
                     messages.push(
-                        <div className='bg-sky-400 rounded-xl p-2 mr-auto' key={i}>
-                            <p>{message.message}</p>
+                        <div className='bg-sky-400 rounded-xl p-2 mr-auto mb-2 max-w-[80%] break-words' key={i}>
+                            <p className='whitespace-pre-wrap break-words max-w-[100%]'>{message.message}</p>
                         </div>
                     )
                 }
@@ -75,15 +81,16 @@ class ChatComponent extends React.Component {
         return (
             <>  
                 <div className='h-10'>
-                    <Link to="/messenger" className='text-xl bg-secondary p-0 px-3 leading-9 rounded-full h-9 text-center hover:scale-110 duration-300 mt-2 left-3 absolute'>&lt;</Link>
+                    <Link to="/messenger" className='text-xl bg-secondary p-0 px-3 leading-9 rounded-full h-9 text-center hover:scale-110 duration-300 mt-2 left-3 fixed z-10'>&lt;</Link>
                 </div>
-                <div className='min-w-screen pl-2 pr-2 pt-2'>
+                <div id="messages" className='min-w-screen pl-2 pr-2 pt-2 pb-12' onLoad={this.scrollToBottom}>
                     {
                         messages
                     }
+                    <span ref={this.messagesEndRef}></span>
                 </div>
-                <textarea className='absolute bottom-[1%] left-[1%] w-[78%] bg-secondary rounded-xl h-10' type="textarea" name='message' onChange={this.onChange} value={this.state.messageToSend} />
-                <button className='absolute bottom-[1%] right-[1%] w-[18%] bg-secondary rounded-xl h-10' onClick={this.sendMessages}>Send</button>
+                <textarea className='fixed bottom-[1%] left-[1%] w-[78%] bg-secondary rounded-xl h-10 z-10' type="textarea" name='message' onChange={this.onChange} value={this.state.messageToSend} />
+                <button className='fixed bottom-[1%] right-[1%] w-[18%] bg-secondary rounded-xl h-10 z-10' onClick={this.sendMessages}>Send</button>
             </>
         );
     }
