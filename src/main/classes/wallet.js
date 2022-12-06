21a022d859602f2375f3a746833a6068513971f1
Michel Barnich <32980248+michelbarnich@users.noreply.github.com>
2022-12-06 15:45:25 +0100

    change wallet if WALLET_FILE is set

--- src/main/classes/wallet.js
+++ src/main/classes/wallet.js
@@ -6,8 +6,9 @@ const Crypto = require("./crypto");
 class Wallet {
     constructor() {
         this._data = {};
-        this.loadWalletData();
         this.crypto;
+        this.walletFileName = (process.env.WALLET_FILE == undefined) ? "wallet.json" : process.env.WALLET_FILE;
+        this.loadWalletData();
     }
 
     loadWalletData() {
@@ -15,11 +16,11 @@ class Wallet {
             if(!fs.existsSync(app.getPath("appData") + "/cypher-wallet/"))
                 fs.mkdirSync(app.getPath("appData") + "/cypher-wallet/")
             
-            if(!fs.existsSync(app.getPath("appData") + "/cypher-wallet/wallet.json")) {
+            if(!fs.existsSync(app.getPath("appData") + "/cypher-wallet/" + this.walletFileName)) {
                 this.crypto = new Crypto();
                 this.data = {balance:0, blockHeight: -1, transactions:[], privateKey: this.crypto.privateKey, blockchainAddress: this.crypto.blockchainAddress, nodeAddress: "127.0.0.1", nodePort: 1234, chats: {}};
             } else {
-                this._data = JSON.parse(fs.readFileSync(app.getPath("appData") + "/cypher-wallet/wallet.json").toString("utf8"));
+                this._data = JSON.parse(fs.readFileSync(app.getPath("appData") + "/cypher-wallet/" + this.walletFileName).toString("utf8"));
                 console.log(this._data)
                 this.crypto = new Crypto(this._data.privateKey);
             }
@@ -40,7 +41,7 @@ class Wallet {
         this._data = data;
 
         try {
-            fs.writeFileSync(app.getPath("appData") + "/cypher-wallet/wallet.json", JSON.stringify(this._data));
+            fs.writeFileSync(app.getPath("appData") + "/cypher-wallet/" + this.walletFileName, JSON.stringify(this._data));
         } catch (error) {
             console.log(error)
         }
